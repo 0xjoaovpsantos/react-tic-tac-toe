bf29bd31efc480b430e03c8863e128116ea78b8a
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-23 14:41:07 -0300

    Updates

---
+++ precache-manifest.e65f8d88f706a506080a13506f080ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1028552b97e8f788f703af42f13f96",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5d71ad257e4ab19a74ae",
+    "url": "/react-tic-tac-toe/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "5389a96a255e959649bf",
+    "url": "/react-tic-tac-toe/static/js/2.1a968083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.1a968083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d71ad257e4ab19a74ae",
+    "url": "/react-tic-tac-toe/static/js/main.db85a1d5.chunk.js"
+  },
+  {
+    "revision": "57208936ddd33bb86d2e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.8a07e9c0.js"
+  }
+]);
